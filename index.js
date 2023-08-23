f986d9d962f68c4f98f4aa20135462836cefcf10
barit <mmyers.com@Gmail.com>
2023-08-23 14:27:28 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const biingoboongo = document.querySelector("#text")
+    biingoboongo.textContent = "This is really cool!"
+});
